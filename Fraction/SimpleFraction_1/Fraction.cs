be0ad2db7b0ca3c5960ac47a894ec1719818e905
Zipper <zipper@gmail.com>
2020-12-17 19:25:18 +0200

    Add projects

---
+++ Fraction/SimpleFraction_1/Fraction.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleFraction_1
+{
+    class Fraction
+    {
+        int denominator = 0;
+        public int Numerator { get; set; }
+        public int Denominator
+        {
+            get
+            {
+                return denominator;
+            }
+            set
+            {
+                if (value != 0)
+                {
+                    denominator = value;
+                }
+            }
+        }
+        public Fraction()
+        {
+
+        }
+        public Fraction(int numerator, int denumerator)
+        {
+            Numerator = numerator;
+            Denominator = denumerator;
+        }
+        public void FillFraction()
+        {
+            Console.Write("Enter numerator's value: ");
+            Numerator = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter denominator's value: ");
+            Denominator = Convert.ToInt32(Console.ReadLine());
+        }
+        public void WriteFraction()
+        {
+            Console.WriteLine($"{Numerator}/{Denominator}");
+        }
+        public void AddFraction(Fraction fraction)
+        {
+            if (Denominator == fraction.Denominator)
+            {
+                Console.WriteLine($"{Numerator + fraction.Numerator}/{fraction.Denominator}");
+            }
+            else
+            {
+                Console.WriteLine($"{(Numerator * fraction.Denominator) + (fraction.Numerator * Denominator)}/{GetLeastCommonMultiple(Denominator, fraction.Denominator)}");
+            }
+        }
+        public void SubtractFraction(Fraction fraction)
+        {
+            if (Denominator == fraction.Denominator)
+            {
+                Console.WriteLine($"{Numerator - fraction.Numerator}/{fraction.Denominator}");
+            }
+            else
+            {
+                Console.WriteLine($"{(Numerator * fraction.Denominator) - (fraction.Numerator * Denominator)}/{GetLeastCommonMultiple(Denominator, fraction.Denominator)}");
+            }
+        }
+        public void MultiplyFraction(Fraction fraction)
+        {
+            Console.WriteLine($"{Numerator * fraction.Numerator}/{Denominator * fraction.Denominator}");
+        }
+        public void DivideFraction(Fraction fraction)
+        {
+            Console.WriteLine($"{Numerator * fraction.Denominator}/{Denominator * fraction.Numerator}");
+        }
+        int GetLeastCommonMultiple(int a, int b)
+        {
+            int gcd(int a, int b)
+            {
+                if (a == 0) return b;
+                while (b != 0)
+                {
+                    int r = 0;
+                    if (a > b)
+                    {
+                        r = a % b;
+                    }
+                    else
+                    {
+                        r = b % a;
+                    }
+                    a = b;
+                    b = r;
+                }
+                return a;
+            }
+
+            return (a * b) / gcd(a, b);
+        }
+
+        public static Fraction operator ++(Fraction fraction)
+        {
+            return new Fraction() { Numerator = fraction.Numerator + fraction.Denominator, Denominator = fraction.Denominator };
+        }
+
+        public static Fraction operator --(Fraction fraction)
+        {
+            return new Fraction() { Numerator = fraction.Numerator - fraction.Denominator, Denominator = fraction.Denominator };
+        }
+    }
+}
---
+++ Fraction/SimpleFraction_1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace SimpleFraction_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWorking = true;
+            char chooser;
+            Fraction firstFraction = new Fraction(2, 6);
+            Fraction secondFraction = new Fraction(1, 4);
+
+            while (isWorking)
+            {
+                Console.WriteLine
+                    (
+                    "\n" +
+                    "1. Enter numbers for fisrtFraction \n" +
+                    "2. Enter numbers for secondFraction \n" +
+                    "3. Add secondFraction to firstFraction \n" +
+                    "4. Substract secondFraction from firstFraction \n" +
+                    "5. Multiply fractions \n" +
+                    "6. Divide firstFraction by secondFraction \n" +
+                    "7. Increment firstFraction by 1 \n" +
+                    "8. Decrement 1 from secondFraction \n" +
+                    "9. Quit \n"
+                    );
+                chooser = Convert.ToChar(Console.ReadLine());
+                switch (chooser)
+                {
+                    case '1':
+                        firstFraction.FillFraction();
+                        break;
+                    case '2':
+                        secondFraction.FillFraction();
+                        break;
+                    case '3':
+                        firstFraction.AddFraction(secondFraction);
+                        break;
+                    case '4':
+                        firstFraction.SubtractFraction(secondFraction);
+                        break;
+                    case '5':
+                        firstFraction.MultiplyFraction(secondFraction);
+                        break;
+                    case '6':
+                        firstFraction.DivideFraction(secondFraction);
+                        break;
+                    case '7':
+                        firstFraction++;
+                        break;
+                    case '8':
+                        secondFraction--;
+                        break;
+                    case '9':
+                        isWorking = false;
+                        break;
+
+                }
+            }
+        }
+    }
+}
